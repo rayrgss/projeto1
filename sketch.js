b3cbd4471430392ec1c7a4dc8d7b3c167af90db0
rayrgss <168771813+rayrgss@users.noreply.github.com>
2024-05-16 15:45:22 -0300

    projeto original

---
+++ sketch.js
@@ -0,0 +1,26 @@
+lavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = p
